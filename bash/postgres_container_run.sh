152179f72844664cc244e26500407d2cefd3ef75
eryk.mikolajewicz <eryk.mikolajewicz@gmail.com>
2026-01-23 22:52:02 +0100

    bugfix bash scripts, env: Match env names with that in settings, and make changes in db launching script, to reflect it.

--- bash/postgres_container_run.sh
+++ bash/postgres_container_run.sh
@@ -1,6 +1,6 @@
-source config/relational_db.env
+source .env
 
 podman run -d --name postgres-prawobiorca \
-  -e POSTGRES_PASSWORD="$PASSWORD" -e POSTGRES_USER="$DB_USER" -e POSTGRES_DB="$DB_NAME" \
-  -p "$PORT":5432 -v postgres-prawobiorca:/var/lib/postgresql/data:Z \
-  postgres:latest
+  -e POSTGRES_PASSWORD="$RELATIONAL_DB_PASSWORD" -e POSTGRES_USER="$RELATIONAL_DB_USER" \
+  -e POSTGRES_DB="$RELATIONAL_DB_NAME" -p "$RELATIONAL_DB_PORT":5432 -v \
+  postgres-prawobiorca:/var/lib/postgresql postgres:18
