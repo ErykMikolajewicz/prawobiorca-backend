3ea7e10e5946c1b0b7986fd2e49d8b43865620d6
martinszkita <marpozniak1@gmail.com>
2026-02-15 22:33:21 +0100

    update document extraction

--- app/framework/api/endpoints/divide_document.py
+++ app/framework/api/endpoints/divide_document.py
@@ -14,19 +14,37 @@ from dotenv import load_dotenv
 from pathlib import Path
 
 def _divide_document()->list:
-    """Returns a list where each element is one chapter of the document"""
+    """Returns a list of chapters of the document"""
     load_dotenv()
     doc_path = Path(os.environ['PWR_REGULAMIN_PDF_URL'])
     doc_str = extract_text(doc_path)
     chapters = []
     
     # pattern for selecting the whole chapter (Rozdział) - header & content
-    regex = r"^(?:Rozdział\s)?[IVX]+[^\.\n]*\n[\s\S]*?(?=^(?:Rozdział\s)?[IVX]+[^\.\n]*|\Z)"
+    regex_chapter = r"^(?:Rozdział\s)?[IVX]+[^\.\n]*\n[\s\S]*?(?=^(?:Rozdział\s)?[IVX]+[^\.\n]*|\Z)"
+    
+    # pattern for selecting only the title of the chapter - e.g. "Rozdział I – Postanowienia ogólne"
+    regex_chapter_title = r"^Rozdział\s[IVX]+[^\.\n§]* "
 
-    matches = re.finditer(regex, doc_str, re.MULTILINE)
+    matches = re.finditer(regex_chapter, doc_str, re.MULTILINE)
     chapters = [match.group() for match in matches]
 
-    return chapters
+    chapter_titles = [str(re.findall(regex_chapter_title, chapter)[0]).strip() or '' for chapter in chapters]
+    pure_chapter_titles = []
+    
+    for i, chapter_title in enumerate(chapter_titles, start=1):
+        print(i, chapter_title)
+        pure_title_regex = r'[-–]\s*(.+)'
+        match = re.search(pure_title_regex, chapter_title)
+        
+        if not match:
+            raise Exception("Rozdział XXX regex error")
+        
+        pure_title = match.group(1).strip()
+            
+        pure_chapter_titles.append([i, pure_title]) 
+  
+    return [pure_chapter_titles[i]+[chapters[i]] for i in range(len(chapters))]
 
 logger = logging.getLogger(__name__)
 
