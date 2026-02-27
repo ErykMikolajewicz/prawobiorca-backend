2c89fe0a3a1d4967e9cbf17437ef655e3753f041
martinszkita <marpozniak1@gmail.com>
2026-02-27 12:59:29 +0100

    added deleting user cases using buttons

---
+++ app/framework/web/static/js/index.js
@@ -0,0 +1,30 @@
+console.log("JS działa");
+
+document.addEventListener("DOMContentLoaded", () => {
+  document
+    .querySelectorAll('[data-bs-toggle="popover"]')
+    .forEach((button) => {
+
+      const caseId = button.closest("li")
+        .querySelector("input[name='case_id']")
+        ?.value;
+
+      const content = `
+        <form action="/user/cases/delete" method="post">
+          <input type="hidden" name="case_id" value="${caseId}">
+          <p class="mb-2">Na pewno usunąć?</p>
+          <button type="submit" class="btn btn-sm btn-danger w-100">
+            Potwierdź
+          </button>
+        </form>
+      `;
+
+      new bootstrap.Popover(button, {
+        html: true,
+        content: content,
+        container: "body",
+        trigger: "focus",
+        sanitize: false,  
+      });
+    });
+});
